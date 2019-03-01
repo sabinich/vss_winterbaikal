f11a16bc85cda152048da9f9a62505920a15cc73
vadim s. sabinich <1430016+sabinich@users.noreply.github.com>
2019-03-01 14:30:25 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'vss_winterbaikal_enqueue_styles' );
+
 function vss_winterbaikal_enqueue_styles() {
  
     $parent_style = 'journalistic-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
@@ -10,7 +11,10 @@ function vss_winterbaikal_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
- wp_enqueue_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.css',true );  
- wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/font-awesome/css/font-awesome.css',true );
+ wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.css',true );  
+ wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri().'/font-awesome/css/font-awesome.css',true );
 }
+
+require_once( get_stylesheet_directory(). '/inc/customizer.php' );
+
 ?>
