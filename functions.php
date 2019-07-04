c267015c9ba4a5150dcb928d8c418124f999a104
vadim s. sabinich <1430016+sabinich@users.noreply.github.com>
2019-07-04 21:44:52 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'vss_winterbaikal_enqueue_styles' );
 
 function vss_winterbaikal_enqueue_styles() {
  
-    $parent_style = 'journalistic-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'journalistic-style';
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -11,8 +11,9 @@ function vss_winterbaikal_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
- wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' );  
- wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
+ wp_enqueue_style( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' );
+ wp_enqueue_script( 'bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',array('jquery'),'',true);
+ wp_enqueue_style( 'font-awesome', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 }
 
 require_once( get_stylesheet_directory(). '/inc/customizer.php' );
